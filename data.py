0940ed85ad2c3f0b084a9040c81ff98889a80fe7
Jared Chapman <thejaredchapman@gmail.com>
2024-02-04 12:25:31 -0600

    random animal facts

---
+++ data.py
@@ -0,0 +1,13 @@
+question_data = [
+    {"text":"Sharks are mammals", "answer": "False"},
+    {"text":"Sea otters have a favorite rock they use to break open food", "answer": "True"},
+    {"text":"The blue whale is the biggest animal to have ever lived", "answer": "True"}, 
+    {"text":"The hummingbird egg is the world's smallest bird egg", "answer": "True"},
+    {"text":"Pigs roll in the mud because they don't like being clean", "answer": "False"},
+    {"text":"Bats are blind", "answer": "False"},    
+    {"text":"A dog sweats by panting its tongue", "answer": "False"}, 
+    {"text":"It takes a sloth two weeks to digest a meal", "answer": "True"}, 
+    {"text":"The largest living from is the Goliath frog of West Africa", "answer": "True"}, 
+    {"text":"An ant can lift 1,000 times its body weight", "answer": "False"}, 
+    {"text":"When exciting a cave, bats always go in the direction of the wind", "answer": "False"}, 
+]
